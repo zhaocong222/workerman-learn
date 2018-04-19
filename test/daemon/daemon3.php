99860be15aec812c5800e6a3de8a34a5fff53780
lemon <422896102@qq.com>
2018-04-19 14:49:50 +0800

    *

--- test/daemon/daemon3.php
+++ test/daemon/daemon3.php
@@ -21,18 +21,6 @@ function daemon(){
     } elseif (0 !== $pid) {
         exit(0);
     }
-
-}
-
-function moniterWorker(){
-    while (1){
-        //父进程阻塞着等待子进程的退出,防止僵死进程
-        //WUNTRACED	 如果子进程进入暂停执行情况则马上返回，但结束状态不予以理会
-        $pid    = pcntl_wait($status, WUNTRACED);
-        if ($pid > 0){
-            echo '子进程是',$pid,',状态是exit';
-        }
-    }
 }
 
 //后台运行
@@ -49,19 +37,32 @@ $redis->connect('127.0.0.1', 6379);
 while (count($pidMap[$_pid]) < $num){
 
     $pid = pcntl_fork();
-    cli_set_process_title('zclemon: work'.count($pidMap[$_pid]));
-    $l = count($pidMap[$_pid]);
 
     if ($pid > 0) {
-        $pidMap[$_pid][] = $l;
+        $pidMap[$_pid][] = count($pidMap[$_pid]);
     } elseif (0 === $pid) {
-
+        cli_set_process_title('zclemon: work'.count($pidMap[$_pid]));
         while (true) {
-            $redis->set("name".$l, "lemon".mt_rand());
+            $redis->set("name".count($pidMap[$_pid]), "lemon".mt_rand());
             sleep(3);
         }
+
+        //子进程退出
+        exit();
+
     } else{
         throw new Exception("forkOneWorker fail");
+    }
+}
+
+function moniterWorker(){
+    while (1){
+        //父进程阻塞着等待子进程的退出,防止僵死进程
+        //WUNTRACED	 如果子进程进入暂停执行情况则马上返回，但结束状态不予以理会
+        $pid    = pcntl_wait($status, WUNTRACED);
+        if ($pid > 0){
+            echo '子进程是',$pid,',状态是exit';
+        }
     }
 }
 
